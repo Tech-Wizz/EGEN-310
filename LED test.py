8dae5235029af46995aecfc3060c85c74c6acbca
Kruize Derek Christensen <55522241+Kruize-Coder@users.noreply.github.com>
2021-11-03 20:33:12 -0600

    Add files via upload

--- LED test.py
+++ LED test.py
@@ -1,12 +1,13 @@
 from gpiozero import LED
 from time import sleep
+import keyboard
 
 led = LED(17)
 
 while True:
-    led.on()
-    print("ON")
-    sleep(1)
+    while keyboard.is_pressed("l"):
+        led.on()
+        print("ON")
     led.off()
     print("OFF")
-    sleep(1)
+    
