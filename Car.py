4c8ae323aba7842f16121cb7ba2a7b1b113de08e
Kruize Derek Christensen <christensen.kruize@gmail.com>
2021-11-04 22:08:39 -0600

    Add files via upload

--- Car.py
+++ Car.py
@@ -5,6 +5,7 @@ import keyboard
 rightMotor = LED(16)
 leftMotor = LED(25)
 pump = LED(24)
+horn = LED(17)
 
 while True:
     while keyboard.is_pressed("w") or keyboard.is_pressed("W"):	
@@ -20,7 +21,11 @@ while True:
     while keyboard.is_pressed("p") or keyboard.is_pressed("P"):
         pump.on()
         print("PUMP")
+    while keyboard.is_pressed("h") or keyboard.is_pressed("H"):
+        horn.on()
+        print("Beep!")
     rightMotor.off()
     leftMotor.off()
     pump.off()
+    horn.off()
 
