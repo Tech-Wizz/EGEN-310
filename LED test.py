ddc61115dca3136d4fad287030182c3087a7ac6c
Kruize Derek Christensen <christensen.kruize@gmail.com>
2021-11-04 18:22:04 -0600

    Update LED test.py
    updated them to example our car

--- LED test.py
+++ LED test.py
@@ -2,12 +2,23 @@ from gpiozero import LED
 from time import sleep
 import keyboard
 
-led = LED(17)
+rightMotor = LED(16)
+leftMotor = LED(25)
+pump = LED(24)
 
 while True:
-    while keyboard.is_pressed("l"):
-        led.on()
-        print("ON")
-    led.off()
+    while keyboard.is_pressed("s"):
+        rightMotor.on()
+        print("RIGHT")
+    while keyboard.is_pressed("a"):
+        leftMotor.on()
+        print("LEFT")
+    while keyboard.is_pressed("w"):
+        leftMotor.on()
+        print("RIGHT")
+    rightMotor.off()
+    leftMotor.off()
+    pump.off()
+
     print("OFF")
     
